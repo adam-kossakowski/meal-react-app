a5fcc13042c49a85fda2250f379fe95632551caf
adako <ad.kossakowski@gmail.com>
2024-03-24 19:29:31 +0100

    addition of CartContext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
+import { useContext } from 'react';
 import logoImg from '../assets/logo.jpg';
 import Button from './UI/Button';
+import CartContext from '../store/CartContext';
 
 export default function Header() {
+
+    const cartCtx = useContext(CartContext);
+
+    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+        return totalNumberOfItems + item.quantity;
+    }, 0);
 
     return (
         <header id="main-header">
@@ -11,7 +19,7 @@ export default function Header() {
             </div>
             <nav>
                 <Button textOnly>
-                    Cart (0)
+                    Cart ({totalCartItems})
                 </Button>
             </nav>
         </header>
