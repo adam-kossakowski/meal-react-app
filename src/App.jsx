d24c8c7fcc6482bd600e34a1b9c7e396eaecec79
Adam Kossakowski <akossakowski@unity-t.pl>
2024-03-26 08:12:08 +0100

    addition of userContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from "./components/Header";
 import Meals from "./components/Meals";
 import { CartContextProvider } from "./store/CartContext";
+import UserProgressContextProvider from "./store/UserProgressContext";
 
 function App() {
   return (
     <CartContextProvider>
-      <Header />
-      <Meals />
+      <UserProgressContextProvider>
+        <Header />
+        <Meals />
+      </UserProgressContextProvider>
     </CartContextProvider>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,12 @@ import { useContext } from 'react';
 import logoImg from '../assets/logo.jpg';
 import Button from './UI/Button';
 import CartContext from '../store/CartContext';
+import UserProgressContext from '../store/UserProgressContext';
 
 export default function Header() {
 
     const cartCtx = useContext(CartContext);
+    const userCtx = useContext(UserProgressContext);
 
     const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
         return totalNumberOfItems + item.quantity;
